f5757cdb4fb3b85664a980f490e5a7835bce0a6b
Sara <sara@gmail.com>
2022-12-06 00:50:08 +0300

    HW

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,51 @@
-import * as React from "react"
+// import * as React from "react"
+// import {
+//   ChakraProvider,
+//   Box,
+//   Text,
+//   Link,
+//   VStack,
+//   Code,
+//   Grid,
+//   theme,
+// } from "@chakra-ui/react"
+
+// export const App = () => (
+//   <ChakraProvider theme={theme}>
+//     <Box textAlign="center" fontSize="xl"></Box>
+//   </ChakraProvider>
+// );
+
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Flex,
+  Heading,
+  Text,
+  VStack,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/login/LoginForm';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <Routes>
+        <Route path='/' element={<LoginPage />} />
+
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+
+          
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
+  );
+};
